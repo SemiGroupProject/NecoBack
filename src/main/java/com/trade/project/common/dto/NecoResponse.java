ce7cb6ad713304b47209be09cb15ad38f6d06fb9
hungrytech <xorals9448@gmail.com>
2021-09-02 21:56:36 +0900

    add: [회원] 예외응답 객체, 핸들러 추가
    * 예외응답객체
    * 예외핸들러

--- src/main/java/com/trade/project/common/dto/NecoResponse.java
+++ src/main/java/com/trade/project/common/dto/NecoResponse.java
@@ -1,7 +1,6 @@
 package com.trade.project.common.dto;
 
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
--- src/main/java/com/trade/project/member/domain/Member.java
+++ src/main/java/com/trade/project/member/domain/Member.java
@@ -2,7 +2,6 @@ package com.trade.project.member.domain;
 
 import com.trade.project.store.domain.Store;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
