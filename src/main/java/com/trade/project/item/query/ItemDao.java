556a75901b589073c6bfe2c57353bc65aac1f4a2
유수완 <yusuwan@malgn.com>
2021-09-11 15:15:09 +0900

    add: [log] log4j2 설정 추가

---
+++ src/main/java/com/trade/project/item/query/ItemDao.java
@@ -0,0 +1,16 @@
+//package com.trade.project.item.query;
+//
+//import com.querydsl.jpa.impl.JPAQueryFactory;
+//
+//import org.springframework.stereotype.Component;
+//
+//
+//@Component
+//public class ItemDao {
+//    private final JPAQueryFactory queryFactory;
+//
+//    public ItemDao(JPAQueryFactory queryFactory) {
+//        this.queryFactory = queryFactory;
+//    }
+//
+//}
