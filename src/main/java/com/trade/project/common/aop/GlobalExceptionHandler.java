e7232d68dc5c7167be708ea0bb0d1cbd119518b2
hungrytech <xorals9448@gmail.com>
2021-09-11 11:24:36 +0900

    refactor: [api Response] 형식 수정
    - 성공과 error 분리

--- src/main/java/com/trade/project/common/aop/GlobalExceptionHandler.java
+++ src/main/java/com/trade/project/common/aop/GlobalExceptionHandler.java
@@ -1,14 +1,13 @@
 package com.trade.project.common.aop;
 
 import com.trade.project.common.dto.ApiUtils;
-import com.trade.project.common.dto.NecoResponse;
-import com.trade.project.common.exceptions.ErrorCode;
 import com.trade.project.common.dto.ErrorResponse;
+import com.trade.project.common.dto.NecoErrorResponse;
 import com.trade.project.common.exceptions.BusinessException;
+import com.trade.project.common.exceptions.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -26,7 +25,7 @@ public class GlobalExceptionHandler {
      * 주로 @RequestBody, @RequestPart 어노테이션에서 발생
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<NecoResponse<ErrorResponse>>  handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    protected ResponseEntity<NecoErrorResponse>  handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         log.error("handleMethodArgumentNotValidException", e);
         final ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, e.getBindingResult());
         return new ResponseEntity<>(ApiUtils.errorResponse(response), HttpStatus.BAD_REQUEST);
@@ -36,7 +35,7 @@ public class GlobalExceptionHandler {
      * 지원하지 않은 HTTP method 호출 할 경우 발생
      */
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    protected ResponseEntity<NecoResponse<ErrorResponse>>  handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+    protected ResponseEntity<NecoErrorResponse>  handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
         log.error("handleHttpRequestMethodNotSupportedException", e);
         final ErrorResponse response = ErrorResponse.of(ErrorCode.METHOD_NOT_ALLOWED);
         return new ResponseEntity<>(ApiUtils.errorResponse(response), HttpStatus.METHOD_NOT_ALLOWED);
@@ -47,7 +46,7 @@ public class GlobalExceptionHandler {
      * Authentication 객체가 필요한 권한을 보유하지 않은 경우 발생합
      */
     @ExceptionHandler(AccessDeniedException.class)
-    protected ResponseEntity<NecoResponse<ErrorResponse>>  handleAccessDeniedException(AccessDeniedException e) {
+    protected ResponseEntity<NecoErrorResponse>  handleAccessDeniedException(AccessDeniedException e) {
         log.error("handleAccessDeniedException", e);
         final ErrorResponse response = ErrorResponse.of(ErrorCode.HANDLE_ACCESS_DENIED);
         return new ResponseEntity<>(ApiUtils.errorResponse(response), HttpStatus.valueOf(ErrorCode.HANDLE_ACCESS_DENIED.getStatus()));
@@ -55,14 +54,14 @@ public class GlobalExceptionHandler {
 
 
     @ExceptionHandler(BusinessException.class)
-    protected ResponseEntity<NecoResponse<ErrorResponse>> handleBusinessException(BusinessException e) {
+    protected ResponseEntity<NecoErrorResponse> handleBusinessException(BusinessException e) {
         log.error("handleEntityNotFoundException", e);
         ErrorCode errorCode = e.getErrorCode();
         return new ResponseEntity<>(ApiUtils.errorResponse(errorCode),HttpStatus.valueOf(errorCode.getStatus()));
     }
 
     @ExceptionHandler(Exception.class)
-    protected ResponseEntity<NecoResponse<ErrorResponse>>  handleException(Exception e) {
+    protected ResponseEntity<NecoErrorResponse>  handleException(Exception e) {
         log.error("Exception", e);
         final ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
         return new ResponseEntity<>(ApiUtils.errorResponse(response), HttpStatus.INTERNAL_SERVER_ERROR);
--- src/main/java/com/trade/project/common/dto/ApiUtils.java
+++ src/main/java/com/trade/project/common/dto/ApiUtils.java
@@ -5,14 +5,14 @@ import com.trade.project.common.exceptions.ErrorCode;
 public class ApiUtils {
 
     public static <T> NecoResponse<T> successResponse(T response) {
-        return new NecoResponse<>(true, response, null);
+        return new NecoResponse<>(true, response);
     }
 
-    public static NecoResponse<ErrorResponse> errorResponse (ErrorCode errorCode){
-        return new NecoResponse<>(false, null, ErrorResponse.of(errorCode));
+    public static NecoErrorResponse errorResponse (ErrorCode errorCode){
+        return new NecoErrorResponse(false, ErrorResponse.of(errorCode));
     }
 
-    public static NecoResponse<ErrorResponse> errorResponse (ErrorResponse errorResponse){
-        return new NecoResponse<>(false, null, errorResponse);
+    public static NecoErrorResponse errorResponse (ErrorResponse errorResponse){
+        return new NecoErrorResponse(false, errorResponse);
     }
 }
